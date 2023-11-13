264abc81aa7b1732b8a87bd6a7cdd0a545a44c30
teddy-choe <aackddlr@gmail.com>
2023-11-14 00:29:07 +0900

    add circular indicator while loading search result

--- lib/page/search_list.dart
+++ lib/page/search_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:search_github/page/repo_page.dart';
 import 'package:search_github/provider/search_provider.dart';
 
 import '../model/User.dart';
@@ -11,19 +12,32 @@ class SearchListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<SearchProvider>(builder: (context, provider, child) {
+      if (provider.isLoading == true) {
+        return const Center(child: CircularProgressIndicator());
+      }
+
+      if (provider.state == null) {
+        return const Center(child: Text("유저를 검색해주세요."));
+      }
+
       return Expanded(
-        child: provider.state == null
-            ? Text("유저를 검색해주세요.")
-            : ListView.builder(
-                itemCount: provider.state!.totalCount,
-                itemBuilder: (context, index) {
-                  User user = provider.state!.users[index];
-                  return ListTile(
-                    leading: Image.network(user.avatarUrl),
-                    title: Text(user.login),
-                    subtitle: Text(user.reposUrl),
-                  );
-                }),
+        child: ListView.builder(
+            itemCount: provider.state!.totalCount,
+            itemBuilder: (context, index) {
+              User user = provider.state!.users[index];
+              return ListTile(
+                leading: Image.network(user.avatarUrl),
+                title: Text(user.login),
+                subtitle: Text(user.reposUrl),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              RepoPage(userName: user.login)));
+                },
+              );
+            }),
       );
     });
   }
--- lib/provider/search_provider.dart
+++ lib/provider/search_provider.dart
@@ -5,22 +5,29 @@ import 'package:logger/logger.dart';
 import 'package:search_github/provider/search_state.dart';
 import 'package:search_github/repository/search_repository.dart';
 
+/**
+ * 검색 화면에서 사용
+ */
 class SearchProvider with ChangeNotifier {
   SearchState? _state;
   var logger = Logger();
+  bool isLoading = false;
 
   final SearchReposiory repository = SearchReposiory();
 
   SearchState? get state => _state;
 
-  Future<bool> search(String query) async {
+  search(String query) async {
+    isLoading = true;
+    notifyListeners();
+
     await repository.search(query).then((value) => {
       _state = SearchState.fromJson(json.decode(value.body))
     });
 
     logger.d(_state?.totalCount);
 
+    isLoading = false;
     notifyListeners();
-    return true;
   }
 }
