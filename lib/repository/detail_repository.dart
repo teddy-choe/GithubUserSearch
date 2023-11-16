7ad53a503bc8da5a7061f3955c20265e9e68a248
teddy-choe <aackddlr@gmail.com>
2023-11-16 11:37:56 +0900

    fix dio duplicated creation

--- lib/repository/detail_repository.dart
+++ lib/repository/detail_repository.dart
@@ -4,33 +4,35 @@ import 'package:flutter/foundation.dart';
 import '../util/network/http_interceptor.dart';
 
 class DetailRepository {
-  final dio = Dio();
+  late Dio _dio;
 
-  void initDio() {
-    dio.httpClientAdapter = HttpClientAdapter();
-    dio.options = BaseOptions(
+  DetailRepository() {
+    _dio = Dio();
+    _initDio();
+  }
+
+  void _initDio() {
+    _dio.httpClientAdapter = HttpClientAdapter();
+    _dio.options = BaseOptions(
       headers: {"Accept": "application/vnd.github+json"},
       connectTimeout: Duration(minutes: 1),
       receiveTimeout: Duration(minutes: 1),
       validateStatus: (_) => true,
     );
-    dio.interceptors.addAll(<Interceptor>[
+    _dio.interceptors.addAll(<Interceptor>[
       if (kDebugMode) (HttpInterceptor())
     ]);
   }
 
   Future<Response> getFollowers(String followerUrl) async {
-    initDio();
-    return await dio.get(followerUrl);
+    return await _dio.get(followerUrl);
   }
 
   Future<Response> getFollowings(String followingUrl) async {
-    initDio();
-    return await dio.get(followingUrl.replaceAll('{/other_user}', ''));
+    return await _dio.get(followingUrl.replaceAll('{/other_user}', ''));
   }
 
   Future<Response> getRepos(String reposUrl) async {
-    initDio();
-    return await dio.get(reposUrl);
+    return await _dio.get(reposUrl);
   }
 }
--- lib/repository/search_repository.dart
+++ lib/repository/search_repository.dart
@@ -5,25 +5,29 @@ import 'package:search_github/util/network/http_interceptor.dart';
 import '../util/const.dart';
 
 class SearchRepository {
-  final dio = Dio();
+  late Dio _dio;
 
-  void initDio() {
-    dio.httpClientAdapter = HttpClientAdapter();
-    dio.options = BaseOptions(
+  SearchRepository() {
+    _dio = Dio();
+    _initDio();
+  }
+
+  void _initDio() {
+    _dio.httpClientAdapter = HttpClientAdapter();
+    _dio.options = BaseOptions(
       baseUrl: baseUrl,
       headers: {"Accept": "application/vnd.github+json"},
       connectTimeout: Duration(minutes: 1),
       receiveTimeout: Duration(minutes: 1),
       validateStatus: (_) => true,
     );
-    dio.interceptors.addAll(<Interceptor>[
+    _dio.interceptors.addAll(<Interceptor>[
       if (kDebugMode) (HttpInterceptor()),
     ]);
   }
 
   Future<Response> search(String query, int page) async {
-    initDio();
-    return await dio.get(
+    return await _dio.get(
       'users',
       queryParameters: {'q': query, 'page': page.toString()},
     );
